45cc4b66c41142f53217b28169f5ef12fd64415a
Jay <jraval93@gmail.com>
2024-05-08 23:08:03 -0400

    Initial Commit

---
+++ assignment_ensemble/assignment_ensembleApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_ensembleApp.swift
+//  assignment_ensemble
+//
+//  Created by Jay Raval on 2024-05-08.
+//
+
+import SwiftUI
+
+@main
+struct assignment_ensembleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
